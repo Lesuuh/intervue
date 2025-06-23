312387013f5a9798042952507de68031a029012a
Lesuuh <lesuuh01@gmail.com>
2025-06-23 01:45:53 +0100

    add the summary page

---
+++ app/(main)/dashboard/_components/Summary.tsx
@@ -0,0 +1,53 @@
+import { title } from "process";
+
+const Summary = () => {
+  const summaryData = [
+    {
+      title: "Total Interviews",
+      value: 12,
+      desc: "+2 this week",
+    },
+    {
+      title: "Upcoming Interviews",
+      value: 6,
+      desc: "4 expiring soon",
+    },
+    {
+      title: "Completed",
+      value: 24,
+      desc: "This month",
+    },
+  ];
+  return (
+    <div>
+      <main className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:flex-row mt-10">
+        {summaryData.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col justify-space-between gap-2 p-6 rounded border bg-white"
+          >
+            <div>
+              <h4 className="font-semibold">{item.title}</h4>
+              <h2 className="text-4xl font-semibold">{item.value}</h2>
+            </div>
+            <div className="text-sm text-gray-500">
+              <p
+                className={`${
+                  item.title === "Total Interviews"
+                    ? "text-green-500"
+                    : item.title === "Upcoming Interviews"
+                    ? "text-red-500"
+                    : "text-gray-500"
+                }`}
+              >
+                {item.desc}
+              </p>
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+};
+
+export default Summary;
--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import RecentInterviews from "./_components/RecentInterviews";
+import Summary from "./_components/Summary";
 
 const Dashboard = () => {
   return (
     <div>
       <RecentInterviews />
+      <Summary />
     </div>
   );
 };

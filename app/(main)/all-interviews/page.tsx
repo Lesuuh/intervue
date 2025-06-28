1fb11031dc175a277e253f1873d4bcf954f0217c
Lesuuh <lesuuh01@gmail.com>
2025-06-28 09:23:19 +0100

    feat: implement Popover component with trigger and content, add Badge component for UI consistency, and update dependencies

--- app/(main)/all-interviews/page.tsx
+++ app/(main)/all-interviews/page.tsx
@@ -7,9 +7,11 @@ import { useAuthStore } from "@/store/useAuthStore";
 import { useQuery } from "@tanstack/react-query";
 import { differenceInDays, format, isBefore } from "date-fns";
 import { Clock, Copy, ExternalLink, MoreHorizontal } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const AllInterviews = () => {
+  const router = useRouter();
   const heading = [
     "Position",
     "duration",
@@ -18,8 +20,6 @@ const AllInterviews = () => {
     "status",
     "actions",
   ];
-
-  const status = ["Expired", "Active"];
 
   const user = useAuthStore((state) => state.user);
 
@@ -39,8 +39,6 @@ const AllInterviews = () => {
     },
     enabled: !!user,
   });
-
-  // get the created day, get the expiring day, if the expiring day is exceeded, set status as expired
 
   if (isLoading) {
     return (
@@ -70,104 +68,127 @@ const AllInterviews = () => {
     <section className="my-5">
       <header>
         <h2 className="text-2xl text-semibold">All Interviews</h2>
-        <p className="text-sm text-gray-500">Manage all your created interviews</p>
+        <p className="text-sm text-gray-500">
+          Manage all your created interviews
+        </p>
       </header>
-      <main className="my-10">
-        <table className="min-w-full border border-gray-200">
-          <thead className="bg-gray-100 border">
-            <tr>
-              {heading.map((tr) => (
-                <th
-                  key={tr}
-                  className="px-6 py-3 text-left text-xs  bg-gray-100  font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  {tr.toUpperCase()}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200">
-            {data?.map((interview) => {
-              const now = new Date();
-              const expiresAt = new Date(interview.expiresAt);
-              const daysLeft = differenceInDays(expiresAt, now);
-
-              let status = "";
-              if (isBefore(expiresAt, now)) {
-                status = "Expired";
-              } else if (daysLeft <= 7) {
-                status = "Expiring Soon";
-              } else {
-                status = "Active";
-              }
-
-              return (
-                <tr key={interview.id} className="hover:bg-gray-50">
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div>
-                      <div className="text-sm font-medium text-gray-900">
-                        {interview.jobPosition}
-                      </div>
-                      <div className="text-sm text-gray-500 truncate max-w-xs">
-                        {interview.jobDescription}
-                      </div>
-                    </div>
-                  </td>
-
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                    {interview.duration} min
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {format(interview.createdAt, "MMM dd, yyyy")}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                      <Clock className="h-4 w-4 mr-1 text-gray-400" />
-                      <span className="text-sm text-gray-500">
-                        {format(interview.expiresAt, "MMM dd, yyy")}
-                      </span>
-                    </div>
-                  </td>
-                  <td
-                    className={`px-6 py-4 whitespace-nowrap text-sm text-gray-900 ${
-                      status === "Active"
-                        ? "text-green-600 "
-                        : status === "Expiring Soon"
-                        ? "text-yellow-500"
-                        : "text-red-500"
-                    }`}
-                  >
-                    {status}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                    <div className="flex items-center space-x-2">
-                      <Button
-                        size="sm"
-                        variant="ghost"
-                        onClick={() => handleCopy(interview.interview_id)}
-                        className="cursor-pointer"
-                      >
-                        <Copy className="h-4 w-4 cursor-pointer" />
-                      </Button>
-                      <a
-                        href={`http://localhost:3000/interview/${interview.interview_id}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Button size="sm" variant="ghost">
-                          <ExternalLink className="h-4 w-4" />
-                        </Button>
-                      </a>
-                      <Button size="sm" variant="ghost">
-                        <MoreHorizontal className="h-4 w-4" />
-                      </Button>
-                    </div>
-                  </td>
+      <main className="my-10 overflow-x-auto ">
+        <div>
+          {data?.length === 0 ? (
+            <div className="flex flex-col items-center border justify-center p-6  bg-white rounded-lg shadow">
+              <h3 className="text-lg font-semibold text-gray-700 mb-2 py-6">
+                No Interviews Yet
+              </h3>
+              <p className="text-gray-500 mb-6 text-center">
+                You haven&apos;t created any interviews yet. Start by creating
+                your first interview!
+              </p>
+              <Button
+                onClick={() => router.push("/dashboard/create-interview")}
+                className="my-6"
+              >
+                Create Interview
+              </Button>
+            </div>
+          ) : (
+            <table className="min-w-full border border-gray-200">
+              <thead className="bg-gray-100 border">
+                <tr>
+                  {heading.map((tr) => (
+                    <th
+                      key={tr}
+                      className="px-6 py-3 text-left text-xs  bg-gray-100  font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      {tr.toUpperCase()}
+                    </th>
+                  ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {data?.map((interview) => {
+                  const now = new Date();
+                  const expiresAt = new Date(interview.expiresAt);
+                  const daysLeft = differenceInDays(expiresAt, now);
+
+                  let status = "";
+                  if (isBefore(expiresAt, now)) {
+                    status = "Expired";
+                  } else if (daysLeft <= 7) {
+                    status = "Expiring Soon";
+                  } else {
+                    status = "Active";
+                  }
+
+                  return (
+                    <tr key={interview.id} className="hover:bg-gray-50">
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div>
+                          <div className="text-sm font-medium text-gray-900">
+                            {interview.jobPosition}
+                          </div>
+                          <div className="text-sm text-gray-500 truncate max-w-xs">
+                            {interview.jobDescription}
+                          </div>
+                        </div>
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {interview.duration} min
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {format(interview.createdAt, "MMM dd, yyyy")}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <Clock className="h-4 w-4 mr-1 text-gray-400" />
+                          <span className="text-sm text-gray-500">
+                            {format(interview.expiresAt, "MMM dd, yyy")}
+                          </span>
+                        </div>
+                      </td>
+                      <td
+                        className={`px-6 py-4 whitespace-nowrap text-sm text-gray-900 ${
+                          status === "Active"
+                            ? "text-green-600 "
+                            : status === "Expiring Soon"
+                            ? "text-yellow-500"
+                            : "text-red-500"
+                        }`}
+                      >
+                        <p>{status}</p>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                        <div className="flex items-center space-x-2">
+                          <Button
+                            size="sm"
+                            variant="ghost"
+                            onClick={() => handleCopy(interview.interview_id)}
+                            className="cursor-pointer"
+                          >
+                            <Copy className="h-4 w-4 cursor-pointer" />
+                          </Button>
+                          <a
+                            href={`http://localhost:3000/interview/${interview.interview_id}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <Button size="sm" variant="ghost">
+                              <ExternalLink className="h-4 w-4" />
+                            </Button>
+                          </a>
+
+                          <Button size="sm" variant="ghost">
+                            <MoreHorizontal className="h-4 w-4" />
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
       </main>
     </section>
   );
